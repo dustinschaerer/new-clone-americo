85c6e60e5334f8240f07e263eb53b180ebfaa6b1
Dustin Schaerer <dustinschaerer@gmail.com>
2013-12-05 10:23:42 -0800

    Add a title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,9 +10,14 @@ describe "Static pages" do
       expect(page).to have_selector('h1', :text => 'Americo')	
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Americo | Home")
+      expect(page).to have_title("Americo")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')	
     end
 
   end 
