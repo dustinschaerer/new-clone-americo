acbae82aadcf5007e835978bfd2abd28dc635bca
Dustin Schaerer <dustinschaerer@gmail.com>
2013-12-18 22:46:02 -0800

    Added user stories integration test

---
+++ test/integration/user_stories_test.rb
@@ -0,0 +1,62 @@
+require 'test_helper'
+
+class UserStoriesTest < ActionDispatch::IntegrationTest
+  fixtures :products
+
+  # A user goes to the index page. They select a product, adding it to their
+  # cart, and check out, filling in their details on the checkout form. When
+  # they submit, an order is created containing their information, along with a
+  # single line item corresponding to the product they added to their cart.
+  
+  test "buying a product" do
+    LineItem.delete_all
+    Order.delete_all
+    vinyl_swatch = products(:vinyl)
+
+    get "/"
+    assert_response :success
+    assert_template "static_pages/home"
+    
+    xml_http_request :post, '/line_items', product_id: vinyl_swatch.id
+    assert_response :success 
+    
+    cart = Cart.find(session[:cart_id])
+    assert_equal 1, cart.line_items.size
+    assert_equal vinyl_swatch, cart.line_items[0].product
+    
+    get "/orders/new"
+    assert_response :success
+    assert_template "new"
+    
+    post_via_redirect "/orders",
+                      order: { name:     "Tim Smith",
+                               email:    "tim@example.org",
+                               ship_street_address: "123 Way Street", 
+                               ship_city: "Vancouver", 
+                               ship_state: "Washington",
+                               ship_country: "United States",
+                               telephone: "555-555-5555" }
+    assert_response :success
+    assert_template "index"
+    cart = Cart.find(session[:cart_id])
+    assert_equal 0, cart.line_items.size
+    
+    orders = Order.all
+    assert_equal 1, orders.size
+    order = orders[0]
+    
+    assert_equal "Tim Smith",       order.name
+    assert_equal "123 Way Street",  order.ship_street_address
+    assert_equal "tim@example.org", order.email
+    assert_equal "Vancouver",       order.ship_city
+    
+    assert_equal 1, order.line_items.size
+    line_item = order.line_items[0]
+    assert_equal vinyl_swatch, line_item.product
+
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ["tim@example.org"], mail.to
+    assert_equal 'staff@americo-test.us', mail[:from].value
+    assert_equal "Americo Swatch Order Confirmation", mail.subject
+  end
+end
