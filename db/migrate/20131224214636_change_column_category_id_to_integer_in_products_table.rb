de479745331cca90fc407d83b0051492f86292cf
Dustin Schaerer <dustinschaerer@gmail.com>
2014-01-08 16:34:35 -0800

    Change migration file to work with PostgreSQL

--- db/migrate/20131224214636_change_column_category_id_to_integer_in_products_table.rb
+++ db/migrate/20131224214636_change_column_category_id_to_integer_in_products_table.rb
@@ -1,5 +1,5 @@
 class ChangeColumnCategoryIdToIntegerInProductsTable < ActiveRecord::Migration
   def change
-	change_column :products, :category_id, :integer
+	change_column :products, :category_id, 'integer USING CAST(category_id AS integer)'
   end
 end
