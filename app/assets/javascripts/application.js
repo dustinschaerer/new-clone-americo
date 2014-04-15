8f3877045d38c13e2a3ec5f67192e38d6efbd099
Dustin Schaerer <dustinschaerer@gmail.com>
2014-04-15 10:45:23 -0700

    serve static assets in production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,19 +23,3 @@
 //= require best_in_place
 //= require best_in_place.purr
 //= require_tree .
-
-$(document).ready(function() {
-    DependentFields.bind()
-});
-
-$(document).ready(function() {
-  jQuery('.best_in_place').best_in_place();
-});
-
-
-$(document).ready(function() {
-	$('#slider').nivoSlider({
-		startSlide:0, //Set starting Slide (0 index)
-		slideshowEnd: function(){$('#slider').data('nivo:vars').stop = true;} 
-	});
-});
