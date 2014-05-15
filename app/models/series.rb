c16f52d534fcadfc2eb3f74353f8b08e5adf289b
Dustin Schaerer <dustinschaerer@gmail.com>
2014-05-15 11:00:07 -0700

    New revisions from ken

--- app/models/series.rb
+++ app/models/series.rb
@@ -12,7 +12,7 @@ class Series < ActiveRecord::Base
 
 	validates :name, :description, :image_url, presence: true
 	validates :image_url, allow_blank: true, format: {
-		with: %r{\A\.(gif|jpg|png)\z}i,
+		with: %r{\.(gif|jpg|png)\z}i,
 		message: 'must be a URL for GIF, JPG or PNG image.'
 	}
     
