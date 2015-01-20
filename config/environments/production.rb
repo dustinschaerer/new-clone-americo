8ec0dc6d2af3e54b2370a82fe66d6a080dddd087
Dustin Schaerer <dustinschaerer@gmail.com>
2015-01-20 11:44:49 -0800

    Update Production.rb precompile declaration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Americo::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( galleria.classic.js *.woff *.eot *.svg *.ttf  application.js application.css admin.css admin.js)
+  config.assets.precompile += %w( galleria.classic.js *.woff *.eot *.svg *.ttf application.js application.css admin.css admin.js)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
