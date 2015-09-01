279011153338e564cd4cc1068dae9ebdbaed19ff
Dustin Schaerer <dustinschaerer@gmail.com>
2015-08-31 21:06:06 -0700

    Add email sender worker

--- app/workers/email_sender.rb
+++ app/workers/email_sender.rb
@@ -9,7 +9,7 @@ class EmailSender
     if email_message.recipients.kind_of?(Array)
       email_message.recipients.each do |relation|
         relation.find_each do |recipient|
-          EmailMessageNotifier.delay.(email_message.name)(recipient)
+          EmailMessageNotifier.delay.(email_message.name, recipient)
         end
       end
     end
