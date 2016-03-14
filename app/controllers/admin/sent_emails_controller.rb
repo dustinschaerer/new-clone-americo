ff6a379a04d208c385d795f2807eab17ab9977d2
Dustin Schaerer <dustinschaerer@gmail.com>
2016-03-14 09:52:54 -0700

    Change user condition in sent email controller

--- app/controllers/admin/sent_emails_controller.rb
+++ app/controllers/admin/sent_emails_controller.rb
@@ -133,8 +133,8 @@ class Admin::SentEmailsController < AdminController
         actual_recipients_hash["#{@list_entity.email}"] = { @list_entity.id => "user"}
         email_to_send = EmailMessage.find(@sent_email.email_message_id)
         if email_to_send.mailer_method == "dynamic_message"
-          EmailMessageNotifier.send(email_to_send.mailer_method, @list_entity, email_to_send).deliver
-          # EmailMessageNotifier.delay_until(@sent_email.sent_at).send(email_to_send.mailer_method, @list_entity, email_to_send)
+          # EmailMessageNotifier.send(email_to_send.mailer_method, @list_entity, email_to_send).deliver
+          EmailMessageNotifier.delay_until(@sent_email.sent_at).send(email_to_send.mailer_method, @list_entity, email_to_send)
         else
              raise "Inside dynamic_message check"
           # EmailMessageNotifier.send(email_to_send.mailer_method, @list_entity, email_to_send).deliver
