8c5ee68f89f9a69be031505c6f7300cf928fb0c1
Dustin Schaerer <dustinschaerer@gmail.com>
2016-05-31 15:23:10 -0700

    Remove Mandrill stats references

--- app/models/email_message.rb
+++ app/models/email_message.rb
@@ -20,12 +20,12 @@ class EmailMessage < ActiveRecord::Base
     end
   end
 
-  def get_mandrill_stats
-    return if mandril_tag.blank?
-    Mandrill::API.new.tags.info(mandril_tag)
-    rescue Mandrill::InvalidTagNameError
-      false
-  end
+  # def get_mandrill_stats
+  #   return if mandril_tag.blank?
+  #   Mandrill::API.new.tags.info(mandril_tag)
+  #   rescue Mandrill::InvalidTagNameError
+  #     false
+  # end
 
   def id_with_template
     "#{id} - #{template}"
